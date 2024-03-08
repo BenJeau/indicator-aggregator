bab2477455b5c5da0dee7312a91c053e675845d9
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-08 07:38:58 -0500

    feat: add nginx frontend related files

---
+++ frontend/nginx/env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Recreate config file
+rm -rf ./env-config.js
+touch ./env-config.js
+
+# Add assignment
+echo "window._env_ = {" >>./env-config.js
+
+# Iterate over environment keys
+env_keys=(VITE_REST_SERVER_BASE_URL VITE_OPENTEL_URL VITE_ADMIN_EMAIL)
+for key in "${env_keys[@]}"; do
+  value=$(printenv $key)
+  # Append configuration property to JS file
+  echo "  $key: \"$value\"," >>./env-config.js
+done
+
+echo "}" >>./env-config.js
