e9df253d83908ed49d42247dce838b40404c7bbe
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-25 16:04:41 -0400

    feat: add server config values for runners

--- backend/migrations/20240211171406_seed_server_config.up.sql
+++ backend/migrations/20240211171406_seed_server_config.up.sql
@@ -7,4 +7,8 @@ VALUES
   ('PROXY_TYPE', 'Proxy Type', 'The type of proxy to use', 'http', 'string', 'Proxy'),
   ('PROXY_VALUE', 'Proxy Value', 'The value of the proxy to use', 'http://localhost:8080', 'string', 'Proxy'),
   ('SSE_KEEP_ALIVE', 'SSE Keep Alive', 'The keep alive interval in seconds for the SSE connection', '60', 'number', 'Server-Sent Events'),
-  ('SSE_NUMBER_CONCURRENT_SOURCE_FETCHING', 'SSE Source Request Concurrency', 'The number of concurrent sources to fetch from', '10', 'number', 'Server-Sent Events');
+  ('SSE_NUMBER_CONCURRENT_SOURCE_FETCHING', 'SSE Source Request Concurrency', 'The number of concurrent sources to fetch from', '10', 'number', 'Server-Sent Events'),
+  ('JAVASCRIPT_RUNNER_GRPC_ADDRESS', 'JavaScript Runner gRPC Address', 'The address of the JavaScript Runner gRPC server', 'localhost:50052', 'string', 'Runners'),
+  ('JAVASCRIPT_RUNNER_ENABLED', 'JavaScript Runner Enabled', 'Whether to enable the JavaScript Runner', 'true', 'boolean', 'Runners'),
+  ('PYTHON_RUNNER_GRPC_ADDRESS', 'Python Runner gRPC Address', 'The address of the Python Runner gRPC server', 'localhost:50051', 'string', 'Runners'),
+  ('PYTHON_RUNNER_ENABLED', 'Python Runner Enabled', 'Whether to enable the Python Runner', 'true', 'boolean', 'Runners'),;
