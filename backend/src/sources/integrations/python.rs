4001b2f4e6a4cb3177b4e41768731be70cb22fb2
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-25 21:38:27 -0400

    feat: add empty sources and python integration/fetching

---
+++ backend/src/sources/integrations/python.rs
@@ -0,0 +1,37 @@
+use async_trait::async_trait;
+use common::FetchDataRequest;
+use tracing::instrument;
+
+use crate::{
+    sources::{FetchState, Indicator, Source},
+    Result,
+};
+
+pub struct Python;
+
+#[async_trait]
+impl Source for Python {
+    #[instrument(skip_all, err)]
+    async fn fetch_data(
+        &self,
+        indicator: &Indicator,
+        state: &FetchState,
+    ) -> Result<serde_json::Value> {
+        let request = FetchDataRequest {
+            source: state.source_id.to_string(),
+            indicator: Some(common::Indicator {
+                data: indicator.data.clone(),
+                kind: indicator.db_kind(),
+            }),
+        };
+
+        let mut client = common::runner_client::RunnerClient::connect(
+            "http://indicator_aggregator_python_runner:50051",
+        )
+        .await?;
+
+        let data = client.fetch_data(request).await?.into_inner().data;
+
+        Ok(serde_json::Value::String(data))
+    }
+}
