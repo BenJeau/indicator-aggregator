6389a98022d94fd9447819300684edd2efe9dfd2
Benoît Jeaurond <benoit@jeaurond.dev>
2024-04-13 22:40:51 -0400

    feat: bundle less languages with the Monaco editor (#37)
    * feat: import less languages from monaco
    
    * feat: add python as language and proper basic editor files
    
    * chore: run prettier

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -3,20 +3,12 @@ import MonacoEditor from "@monaco-editor/react";
 
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
     if (label === "json") {
       return new jsonWorker();
-    }
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker();
-    }
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker();
     }
     if (label === "typescript" || label === "javascript") {
       return new tsWorker();
@@ -26,7 +18,11 @@ self.MonacoEnvironment = {
 };
 
 import { loader } from "@monaco-editor/react";
-import * as monaco from "monaco-editor";
+import "monaco-editor/esm/vs/language/json/monaco.contribution";
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution";
+import "monaco-editor/esm/vs/basic-languages/python/python.contribution";
+import "monaco-editor/esm/vs/editor/editor.all";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
 loader.config({ monaco });
 
